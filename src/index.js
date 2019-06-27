04463b9146bb0b619d129fbd092234c328c0259c
Marcelo Zapaia <marce@marce.local>
2019-06-27 15:23:22 -0300

    thirs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  background-color: red;
+`;
 
 export default class extends Component {
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <div>
+        <Title>Test</Title>
+        <h2>Welcome to React components</h2>
+      </div>
+    );
   }
 }
